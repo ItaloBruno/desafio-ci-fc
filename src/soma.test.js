ac0998b7d17b5662c44f01e0644f9bf581d3b66f
ItaloBruno <rt.italo.bruno.silva@gmail.com>
2025-08-20 14:48:03 -0300

    erro em importação

--- src/soma.test.js
+++ src/soma.test.js
@@ -1,4 +1,4 @@
-const operacaoDesoma = require("./operacoes")
+const operacaoDesoma = require("./soma")
 
 test("Somar 1 + 4 tem que ser igual a 5", () => {
     expect(operacaoDesoma(1, 4)).toBe(5)
